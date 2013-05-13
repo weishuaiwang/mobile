6d78c3ce58645627e4a5fa1873d603ad29ea2ec5
Vince <ws.file@gmail.com>
2013-05-13 21:23:13 +0800

    Create tap.js

---
+++ tap.js
@@ -0,0 +1,31 @@
+(function($) 
+{
+    $.fn.tap = function(callback)
+    {
+        this.each(function()
+        {
+            var $this = $(this);
+
+            $this.bind('touchstart', function(e){
+                $this.data('moved', false);
+            }).bind('touchmove', function(e){
+                $this.data('moved', true);
+            }).bind('touchend', function(e){
+                if ($this.data('moved') == false)
+                {
+                    if (typeof callback == 'function') 
+                    {
+                        callback.call($this);
+                    }
+                }
+            });
+        });
+
+        return this;
+    }
+})(jQuery);
+
+/* Usage */
+$('#sidebar a').tap(function(){
+    console.log('Tapped!');
+});
